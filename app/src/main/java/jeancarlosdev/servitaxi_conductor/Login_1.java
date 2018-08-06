ef74a390fc6cb8d79002fd528234d7256640c103
Jean Carlos Alarcón <jeancarlosalarguez16@gmail.com>
2018-08-05 23:05:41 -0500

    Add Login View
    Methods Sign in and Register new Taxi Driver

--- app/src/main/java/jeancarlosdev/servitaxi_conductor/Login_1.java
+++ app/src/main/java/jeancarlosdev/servitaxi_conductor/Login_1.java
@@ -1,13 +1,160 @@
 package jeancarlosdev.servitaxi_conductor;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class Login_1 extends AppCompatActivity {
+
+    Button btnSignIn, btnRegister;
+
+    RelativeLayout layoutPrincipal;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CalligraphyConfig.initDefault(new CalligraphyConfig.
+                Builder()
+                .setDefaultFontPath("fonts/Arkhip_font.ttf").
+                        setFontAttrId(R.attr.fontPath).build());
         setContentView(R.layout.frm_login_1);
+
+        auth = FirebaseAuth.getInstance();
+
+
+        btnSignIn = (Button)findViewById(R.id.btnIniciar);
+
+        btnRegister = (Button)findViewById(R.id.btnRegistrar);
+
+        layoutPrincipal = (RelativeLayout)findViewById(R.id.layoutPrincipal);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarVentanaRegistro();
+            }
+        });
+    }
+
+    private void mostrarVentanaRegistro() {
+
+        final AlertDialog.Builder dialog=  new AlertDialog.Builder(this);
+        dialog.setTitle("Registrar");
+        dialog.setMessage("Please use email to register");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View register_Layout = inflater.
+                inflate(R.layout.layout_registrar, null);
+
+        final MaterialEditText etxtEmail = register_Layout.findViewById(
+                R.id.etxtEmail);
+
+        final MaterialEditText etxtContrasena = register_Layout.findViewById(
+                R.id.etxtContrasena);
+
+        final MaterialEditText etxtTelefono = register_Layout.findViewById(
+                R.id.etxtTelefono);
+
+        final MaterialEditText etxtNombre = register_Layout.findViewById(
+                R.id.etxtNombre);
+
+        dialog.setView(register_Layout);
+
+        dialog.setPositiveButton("REGISTRAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                dialogInterface.dismiss();
+                if(TextUtils.isEmpty(etxtEmail.getText().toString())){
+
+                    Snackbar.make(layoutPrincipal,
+                            "Por favor ingrese una direccion de correo " +
+                                    "electronico",
+                            Snackbar.LENGTH_SHORT).show();
+
+                    return;
+                }
+
+                if(etxtContrasena.getText().toString().length() < 6){
+
+                    Snackbar.make(layoutPrincipal,
+                            "Por favor ingrese una contrasena con mas de " +
+                                    "6 caracteres ",
+                            Snackbar.LENGTH_SHORT).show();
+
+                    return;
+                }
+
+                if(TextUtils.isEmpty(etxtNombre.getText().toString())){
+
+                    Snackbar.make(layoutPrincipal,
+                            "Por favor ingrese su Nombre ",
+                            Snackbar.LENGTH_SHORT).show();
+
+                    return;
+                }
+
+
+                if(TextUtils.isEmpty(etxtTelefono.getText().toString())){
+
+                    Snackbar.make(layoutPrincipal,
+                            "Por favor ingrese su telefono de celular ",
+                            Snackbar.LENGTH_SHORT).show();
+
+                    return;
+                }
+
+                Toast.makeText(getApplicationContext(), "Se ha guardado correctamente", Toast.LENGTH_SHORT).show();
+
+              /*  auth.createUserWithEmailAndPassword(etxtEmail.getText().toString(),
+                        etxtContrasena.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+
+                        //Se ha guardado correctamente el Chofer.
+
+
+
+                    }
+                }); */
+            }
+        });
+
+
+        dialog.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 }
